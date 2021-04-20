b7cf59cb435a88525aa459e841c7fc0c0d0eac81
shaverschuren <s.h.a.verschuren@student.tue.nl>
2021-04-20 19:02:34 +0200

    Removed redundant code

--- util/general.py
+++ util/general.py
@@ -2,31 +2,6 @@ import sys
 import json
 
 
-def print_result(succeeded=True):
-
-    if succeeded:
-        print("\033[92mOK\033[0m")
-    elif not succeeded:
-        print("\033[91mFAIL\033[0m")
-    else:
-        raise ValueError("Parameter 'succeeded' should be a boolean")
-    
-    return
-
-
-class print_style:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
-
-
 def check_os():
     """
     This function checks the operating system of the current machine.
