980efadea7862b24c13060568c9633f428f80f56
shaverschuren <s.h.a.verschuren@student.tue.nl>
2021-04-20 12:55:14 +0200

    Implemented .json configuration file + reading

--- src/main.py
+++ src/main.py
@@ -3,17 +3,23 @@ if "" not in sys.path : sys.path.append("")
 if "src" not in sys.path : sys.path.append("src")
 
 import os
+import json
+import nibabel
 import util.general
 
 
-def main():
+def main(config_path):
     """
     Main function for the DBSplan software pipeline.
     TODO: Parameters to pass + processing steps etc. (Might do this in bash eventually)
     """
 
+    # Determine OS
     os_str = util.general.check_os()
+    
+    # Extract config data
+    config_data = util.general.extract_json(config_path)
 
 
 if __name__ == "__main__":
-    main()
+    main("config.json")
--- util/general.py
+++ util/general.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 
 def check_os():
@@ -7,7 +8,7 @@ def check_os():
     It outputs a string, being either 'lnx' : Linux, 'win' : Windows, 'mac' : MacOS.
     Other operating systems are not supported. 
     """
-    
+
     if sys.platform.startswith('win32'):
         os_str = "win"
     elif sys.platform.startswith('linux'):
@@ -18,3 +19,16 @@ def check_os():
         raise ValueError(f"Operating System ({sys.platform}) not supported.")
 
     return os_str
+
+
+def extract_json(json_path, verbose=False):
+    """
+    This function is used for extracting data from .json files.
+    Primarily, it is used for extracting config file data.
+    """
+    with open(json_path) as json_data_file:
+        data = json.load(json_data_file)
+
+    if verbose : print(data)
+
+    return data
