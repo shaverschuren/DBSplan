dee21562527e529f2d7a6d1d1da5c39a14da11b1
shaverschuren <s.h.a.verschuren@student.tue.nl>
2021-06-11 14:17:15 +0200

    Did some work on GUI prototyping Got pyqtgraph widgets in a PyQt main window working Will start on implementing the actual UI now

---
+++ src/gui/tryout.py
@@ -0,0 +1,15 @@
+from PyQt5 import QtWidgets, uic
+import sys
+import os
+
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi(os.path.join("gui", "ui_files", "tryout1.ui"), self)
+        self.show()
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
